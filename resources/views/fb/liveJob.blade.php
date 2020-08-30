6032a2de6b9b8f4f7de019e9b3bb9186d3ba94a2
EC2 Default User <ec2-user@ip-172-31-29-152.us-east-2.compute.internal>
2020-08-30 06:25:43 +0000

    mm

--- resources/views/fb/liveJob.blade.php
+++ resources/views/fb/liveJob.blade.php
@@ -11,12 +11,13 @@
     <body>
         <header>
             <img src="{{ asset('image/lp_instagram_job.png') }}">
-            <div class="headline">
-                <h5>最大<span>『時給3000~4000円』</span>以上 <i class="far fa-hand-point-up"></i></h5>
-                <br>
-                <h5>面倒な通勤必要無し<i class="far fa-hand-point-up"></i><span>「在宅ワーク』</span></h5>
-                <br>
-                <h5><span>好きな時<i class="far fa-clock"></i>に働ける</span>「スキマバイト』</h5>
+            <div class="box26">
+                <span class="box-title">POINT <i class="far fa-hand-point-up"></i></span>
+                <ul>
+                    <li>最大<span class="box26-span">『時給3000~4000円』</span>以上</li>
+                    <li>面倒な通勤<i class="far fa-times-circle"></i> <span class="box26-span">『在宅ワーク』</span></li>
+                    <li><span class="box26-span">『好きな時に働ける』</span>スキマバイト</li>
+                </ul>
             </div>
             <div class="button">
                 <a href="#" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式ページはコチラ <i class="fas fa-external-link-alt"></i></a>
