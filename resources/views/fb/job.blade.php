cbcf1f548fe72f7b3c9597c70e7f1a291f272b02
EC2 Default User <ec2-user@ip-172-31-29-152.us-east-2.compute.internal>
2020-09-14 06:31:05 +0000

    Mm

--- resources/views/fb/job.blade.php
+++ resources/views/fb/job.blade.php
@@ -24,61 +24,45 @@
             
             <div class="big-body-main">
                 <div class="body-main">
-                    <h1>お話好きは必見!?<br>巷で話題の『在宅スキマバイト』ってどういうの？</h1>
+                    <h1>おしゃべり好きは必見!?<br>巷で話題の『在宅スキマバイト』って何...!？</h1>
                     <p>2020年09月14日15:02</p>
                 </div>
                 
                 <div class="body-main-2">
-                    <!--<img src="{{ asset('image/gf_1.jpg') }}">-->
                     <img src="{{ asset('image/jobManga_1.png') }}">
                     <img src="{{ asset('image/jobManga_2.png') }}">
-                    <!--<img src="https://trading-ad.net/a-trade/banner/biscor_600x500_jikyu_main.png">-->
-                    <!--ボタン-->
-                    <!--<a href="https://a-trade.jp/redirect/giftchat?media=G16531" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-arrow-circle-right"></i></a>-->
                 </div>
                 
                 <div class="body-main-3">
-                    <h3>時給4000円超え？自宅で出来るスキマバイト</h3>
-                    <p>今有名雑誌にも掲載されるなど、巷で話題になりつつあるスキマバイトです！<br>お手持ちのスマホを使って、専用SNSを使うだけの簡単なバイト！でも、しっかり報酬が高いのが魅力なお仕事です♬</p>
+                    <h3>時給3,000円超え..!？<br>自宅で出来るお小遣い稼ぎ!</h3>
+                    <p>今有名雑誌にも掲載されるなど、巷で話題になりつつある在宅スキマバイト。<br>お手持ちのスマホを使って、<span class="red">空いた1時間からお仕事可能</span>なのが魅力。また、しっかり報酬が高いのも特徴。</p>
                     <!--<br>-->
-                    <h3>仕事は専用アプリで電話やチャットするだけ！</h3>
-                    <p>LINEや斉藤さんアプリの様に使い、専用アプリ内でユーザーとコミュニケーションするといったもの！<br>イメージはLINEや斉藤さんの様な感じで、ユーザーとコミュニケーションを取ってもらう形です。<br><br>これなら在宅は勿論、TV・SNSを見ながら片手間にお小遣い稼ぎ出来ます。</p>
+                    <h3>お仕事はおしゃべりするだけ..!?<br>悩めるユーザーの相談に乗ろう!</h3>
+                    <p>お話は専用アプリを通じて行い、<span class="red">LINE感覚で友達と話す感じでOK</span>。<br>ユーザーの悩みに乗ったり、気になる話題やあなたの話を聞いてもらおう!</p>
                     <!--<br>-->
-                    <h3>報酬は嬉しい翌日支払い！</h3>
+                    <h3>報酬は嬉しい即日払い！</h3>
                     <img src="{{ asset('image/gf_2.jpg') }}"></img>
                     <br>
                     <!--<br>-->
-                    <h3>巷で話題、アプリバイトが選ばれる理由とは？</h3>
+                    <h3>激レアバイトと呼ばばれる理由とは..!?<br>応募ユーザーが多過ぎる!</h3>
+                    <p>まずなんといってもその好条件が人気の秘密。高時給、在宅ワークは勿論、ファンを見つけ<span class="red">インフルエンサーの足がけ</span>として働く人も多数。</p>
                     <img src="{{ asset('image/gf_3.jpg') }}"></img>
                     <br>
                     <!--<br>-->
-                    <h3>公式ページで詳細をCHECK！</h3>
-                    <p>まだイマイチどんな仕事かピンときていない方が多いかと思います！<br>少しでも気になる方は公式ページ・そして専用アプリ内をチェックしてみて下さい！</p>
+                    <h3>只今、期間限定募集中..!?<br>公式ページで詳細をCHECK!</h3>
+                    <p>そんな激レア求人が、現在期間限定募集中。応募多数につき<span class="red">途中で募集停止するかもしれない</span>ので、気になる方は公式ページをCHECK。</p>
                     <!--ボタン-->
-                    <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8218&site_id=27669" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-arrow-circle-right"></i></a>
-                    <!--<a href="https://a-trade.jp/redirect/giftchat?media=G16531" target="_blank"><span>https://apps.apple.com/jp/app/<br>official/job/gifcha</a></span></a>-->
+                    <a href="https://trading-ad.net/a-trade/banner/moa/02/01_600x500.png" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> お仕事詳細はコチラ <i class="fas fa-arrow-circle-right"></i></a>
                     <br>
-                    <h3>アプリ内にある仕事詳細をCHECK <i class="fas fa-arrow-circle-up"></i></h3>
                     <br>
-                    <img src="{{ asset('image/sukiJob_ad.jpg') }}"></img>
-                    <p>これな↑</p>
-                    <p class="red">※仕事詳細は【必ずアプリ内をCHECK】して下さい <i class="far fa-hand-point-up"></i></p>
+                    <a href="https://a-trade.jp/redirect/moa?media=G16531" target="_blank"><img src="https://trading-ad.net/a-trade/banner/moa/02/01_640x200.png" class="wide"></a>
                     <br>
-                    <p>ちょこっと宣伝！<br>Twitterで高時給 & レアバイトとか(不定期 & 即削除)呟いてます！<br>Twitter：@work_info_</p>
                     <br>
                 </div>
             </div>
-            
-            <!--<div class="ad">-->
-            <!--    <h1><span>【PR】3日で10万円稼ぐ！<br>老舗パパ活アプリで安全に太パパを探す！</span></h1>-->
-            <!--    <a href="https://www.gamefeat.net/webapi/v1/reportClick?ad_id=8218&site_id=27669" target="_blank"><img src="{{ asset('image/sukiJob_ad.jpg') }}"></img></a>-->
-            <!--    <p><span>タップ！<i class="far fa-hand-point-up"></i></span></p>-->
-            <!--    <p><span>専用マッチングアプリから無料で太パパを探す！</span></p>-->
-            <!--</div>-->
-            <!--<br>-->
         </div>
         <footer>
-            <p>激ヤバ話題まとめ@5ch</p>
+            <p>『激レア』バイト話題まとめ@5ch</p>
         </footer>
     </body>
 </html>
